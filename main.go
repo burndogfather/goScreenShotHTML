599eb1d02c5f21084eac5e875307d763e0cb0d4c
coco <squarehacker@kakao.com>
2022-06-23 22:17:34 +0900

    2022-06-23 22:17:34 업데이트!!!

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"time"
+	"github.com/chromedp/cdproto/emulation"
+	"github.com/chromedp/cdproto/page"
+	"github.com/chromedp/chromedp"
+)
+
+func main() {
+	taskCtx, cancel := chromedp.NewContext(
+		context.Background(),
+		chromedp.WithLogf(log.Printf),
+	)
+	defer cancel()
+	taskCtx, cancel = context.WithTimeout(taskCtx, 15*time.Second)
+	defer cancel()
+	var pdfBuffer []byte
+	if err := chromedp.Run(taskCtx, pdfGrabber("https://www.naver.com", "body", &pdfBuffer)); err != nil {
+		log.Fatal(err)
+	}
+	if err := ioutil.WriteFile("naver.pdf", pdfBuffer, 0644); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func pdfGrabber(url string, sel string, res *[]byte) chromedp.Tasks {
+
+	start := time.Now()
+	return chromedp.Tasks{
+		emulation.SetUserAgentOverride("WebScraper 1.0"),
+		chromedp.Navigate(url),
+		chromedp.WaitVisible(`body`, chromedp.ByQuery),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			buf, _, err := page.PrintToPDF().WithPrintBackground(true).Do(ctx)
+			if err != nil {
+				return err
+			}
+			*res = buf
+			fmt.Printf("\nDuration: %f secs\n", time.Since(start).Seconds())
+			return nil
+		}),
+	}
+}
