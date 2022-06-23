522c195b6dc57796279cdeac7bc73ebb183e8d03
coco <squarehacker@kakao.com>
2022-06-24 00:53:13 +0900

    2022-06-24 00:53:13 업데이트!!!

--- main.go
+++ main.go
@@ -65,7 +65,6 @@ func requestHandler(res http.ResponseWriter, req *http.Request) {
 		}
 		*/
 		resdata["status"] = "ok"
-		resdata["pdf"] = BytesToString(pdfBuffer)
 		
 		
 		//반환데이터를 json으로 변환
@@ -108,11 +107,4 @@ func pdfGrabber(url string, sel string, res *[]byte) chromedp.Tasks {
 			return nil
 		}),
 	}
-}
-
-
-func BytesToString(b []byte) string {
-	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
-	sh := reflect.StringHeader{bh.Data, bh.Len}
-	return *(*string)(unsafe.Pointer(&sh))
 }
