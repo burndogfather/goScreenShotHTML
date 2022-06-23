2ded917eea68783982c0597a17a0f8f367c5b7e5
coco <squarehacker@kakao.com>
2022-06-24 00:25:13 +0900

    2022-06-24 00:25:13 업데이트!!!

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func requestHandler(res http.ResponseWriter, req *http.Request) {
 		*/
 		
 		fmt.Println(postdata["url"])
-		
+		fmt.Print(string(postdata[string("url")]))  
 		
 		for _, url := range postdata["url"] {
 			
